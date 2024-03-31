bb6368d13af17f873f00a226219aab8be9c0a477
Jay Potter <j@ypotter.ca>
2024-03-31 12:46:10 -0400

    emitter

--- src/Potter/Event/EventTrait.php
+++ src/Potter/Event/EventTrait.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 
 namespace Potter\Event;
 
+use \Potter\Event\Emitter\EmitterInterface;
+
 trait EventTrait
 {
+    private EmitterInterface $emitter;
     private string $id;
+    
+    final public function getEmitter(): EmitterInterface
+    {
+        return $this->emitter;
+    }
+    
+    final protected function setEmitter(EmitterInterface $emitter): void
+    {
+        $this->emitter = $emitter;
+    }
     
     final public function getId(): string
     {
